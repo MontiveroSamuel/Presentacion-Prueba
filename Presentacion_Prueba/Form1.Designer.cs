66c3bfe45fa4dbcc2962dc814ed06afe1b2d0bff
Montivero Samuel <montiverosamuel@gmail.com>
2021-11-23 00:27:02 -0300

    tercer commit, ahora con buscador dinamico jejox

--- Presentacion_Prueba/Form1.Designer.cs
+++ Presentacion_Prueba/Form1.Designer.cs
@@ -29,12 +29,23 @@ namespace Presentacion_Prueba
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnConectar = new System.Windows.Forms.Button();
+            this.diariosDataSet = new Presentacion_Prueba.DiariosDataSet();
+            this.clientesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.clientesTableAdapter = new Presentacion_Prueba.DiariosDataSetTableAdapters.ClientesTableAdapter();
+            this.dGridViewClientes = new System.Windows.Forms.DataGridView();
+            this.txtBuscador = new System.Windows.Forms.TextBox();
+            this.lblBuscador = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.diariosDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clientesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dGridViewClientes)).BeginInit();
             this.SuspendLayout();
             // 
             // btnConectar
             // 
-            this.btnConectar.Location = new System.Drawing.Point(197, 95);
+            this.btnConectar.Location = new System.Drawing.Point(505, 73);
             this.btnConectar.Name = "btnConectar";
             this.btnConectar.Size = new System.Drawing.Size(75, 23);
             this.btnConectar.TabIndex = 0;
@@ -42,21 +53,86 @@ namespace Presentacion_Prueba
             this.btnConectar.UseVisualStyleBackColor = true;
             this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
             // 
+            // diariosDataSet
+            // 
+            this.diariosDataSet.DataSetName = "DiariosDataSet";
+            this.diariosDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // clientesBindingSource
+            // 
+            this.clientesBindingSource.DataMember = "Clientes";
+            this.clientesBindingSource.DataSource = this.diariosDataSet;
+            // 
+            // clientesTableAdapter
+            // 
+            this.clientesTableAdapter.ClearBeforeFill = true;
+            // 
+            // dGridViewClientes
+            // 
+            this.dGridViewClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGridViewClientes.Location = new System.Drawing.Point(60, 133);
+            this.dGridViewClientes.Name = "dGridViewClientes";
+            this.dGridViewClientes.Size = new System.Drawing.Size(520, 150);
+            this.dGridViewClientes.TabIndex = 1;
+            // 
+            // txtBuscador
+            // 
+            this.txtBuscador.Location = new System.Drawing.Point(158, 73);
+            this.txtBuscador.Name = "txtBuscador";
+            this.txtBuscador.Size = new System.Drawing.Size(164, 20);
+            this.txtBuscador.TabIndex = 2;
+            this.txtBuscador.TextChanged += new System.EventHandler(this.txtBuscador_TextChanged);
+            // 
+            // lblBuscador
+            // 
+            this.lblBuscador.AutoSize = true;
+            this.lblBuscador.Location = new System.Drawing.Point(108, 76);
+            this.lblBuscador.Name = "lblBuscador";
+            this.lblBuscador.Size = new System.Drawing.Size(44, 13);
+            this.lblBuscador.TabIndex = 3;
+            this.lblBuscador.Text = "Nombre";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "BUSCADOR POR NOMBRE";
+            // 
             // PruebaConexion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(346, 251);
+            this.ClientSize = new System.Drawing.Size(644, 338);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblBuscador);
+            this.Controls.Add(this.txtBuscador);
+            this.Controls.Add(this.dGridViewClientes);
             this.Controls.Add(this.btnConectar);
             this.Name = "PruebaConexion";
             this.Text = "Prueba de Conexión";
+            this.Load += new System.EventHandler(this.PruebaConexion_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.diariosDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clientesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dGridViewClientes)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnConectar;
+        private DiariosDataSet diariosDataSet;
+        private System.Windows.Forms.BindingSource clientesBindingSource;
+        private DiariosDataSetTableAdapters.ClientesTableAdapter clientesTableAdapter;
+        private System.Windows.Forms.DataGridView dGridViewClientes;
+        private System.Windows.Forms.TextBox txtBuscador;
+        private System.Windows.Forms.Label lblBuscador;
+        private System.Windows.Forms.Label label1;
     }
 }
 
